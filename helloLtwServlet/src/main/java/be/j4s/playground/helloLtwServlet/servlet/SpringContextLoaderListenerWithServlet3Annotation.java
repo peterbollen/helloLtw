759a297dcf8bd77427546f1ebe364a0885bd8cec
Peter Bollen <peterbollen@gmail.com>
2011-12-05 17:40:36 +0100

    ltw working
    ltw working:
    - moved from spring-instrument-...jar to aspectj-weaver.jar

--- helloLtwServlet/src/main/java/be/j4s/playground/helloLtwServlet/servlet/SpringContextLoaderListenerWithServlet3Annotation.java
+++
@@ -1,7 +0,0 @@
-package be.j4s.playground.helloLtwServlet.servlet;
-
-import javax.servlet.annotation.WebListener;
-
-@WebListener
-public class SpringContextLoaderListenerWithServlet3Annotation extends org.springframework.web.context.ContextLoaderListener {
-}
